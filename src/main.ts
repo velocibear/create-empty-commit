3d135eb5b11c6a0c011ad22db9517385613564f1
Chris James <velocibear@users.noreply.github.com>
2022-03-09 17:03:41 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,22 @@ import { getInput, setOutput, setFailed, info } from "@actions/core";
 
 async function run(): Promise<void> {
   try {
-    const token = core/getInput("token");
-		const message = getInput("message");
-		const email = getInput("email");
-		const name = getInput("name");
-		const octokit = github.getOctokit(token);
+	const token = core/getInput("token");
+	const message = getInput("message");
+	const email = getInput("email");
+	const name = getInput("name");
+	const octokit = github.getOctokit(token);
     
-    const eventPath = process.env.GITHUB_EVENT_PATH;
-		if (!eventPath) {
-			throw new Error("EventPath not reserved");
-		}
-		const event = require(eventPath);
+    	const eventPath = process.env.GITHUB_EVENT_PATH;
+	if (!eventPath) {
+		throw new Error("EventPath not reserved");
+	}
+	const event = require(eventPath);
     
-    const commit_sha = (event?.pull_request?.head?.sha || process.env.GITHUB_SHA) as string;
-		const ref = `heads/${event?.pull_request?.head?.ref as string}`;
-		const full_repository = process.env.GITHUB_REPOSITORY as string;
-		const [owner, repo] = full_repository.split("/");
+    	const commit_sha = (event?.pull_request?.head?.sha || process.env.GITHUB_SHA) as string;
+    	const ref = `heads/${event?.pull_request?.head?.ref as string}`;
+   	const full_repository = process.env.GITHUB_REPOSITORY as string;
+    	const [owner, repo] = full_repository.split("/");
     
     info(`ref ${ref}`);
     
