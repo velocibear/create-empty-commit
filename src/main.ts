dd18de5bc5a025e0f60abbb2a2f7cab24799f93f
Chris James <velocibear@users.noreply.github.com>
2022-03-09 17:02:34 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import { getInput, setOutput, setFailed, info } from "@actions/core";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const token = core/getInput("token");
+		const message = getInput("message");
+		const email = getInput("email");
+		const name = getInput("name");
+		const octokit = github.getOctokit(token);
+    
+    const eventPath = process.env.GITHUB_EVENT_PATH;
+		if (!eventPath) {
+			throw new Error("EventPath not reserved");
+		}
+		const event = require(eventPath);
+    
+    const commit_sha = (event?.pull_request?.head?.sha || process.env.GITHUB_SHA) as string;
+		const ref = `heads/${event?.pull_request?.head?.ref as string}`;
+		const full_repository = process.env.GITHUB_REPOSITORY as string;
+		const [owner, repo] = full_repository.split("/");
+    
+    info(`ref ${ref}`);
+    
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
