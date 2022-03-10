a2577809d70a4120f51ab840f85370f65201e0be
Chris James <chris@lattice.com>
2022-03-09 18:13:45 -0800

    updating

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import * as github from '@actions/github'
-import {info, setFailed} from '@actions/core'
+import {getInput, info, setFailed} from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    // const token = getInput('token')
-    // const message = getInput('message')
-    // const email = getInput('email')
+    const token = getInput('token')
+    const message = getInput('message')
+    const email = getInput('email')
     // const name = getInput('name')
-    // const octokit = github.getOctokit(token)
+    const octokit = github.getOctokit(token)
 
     const payload = github.context.payload
+    const repo = payload.repository
+    const owner = payload.repository?.owner
+    const commit_sha = payload.merge_commit_sha
+    const ref = payload?.pull_request?.head.ref
+
+    info(`owner ${owner}`)
+    info(`commit_sha ${commit_sha}`)
+    info(`ref ${ref}`)
 
     // const commit_sha = (event?.pull_request?.head?.sha ||
     //   process.env.GITHUB_SHA) as string
@@ -17,7 +25,24 @@ async function run(): Promise<void> {
     // const full_repository = process.env.GITHUB_REPOSITORY as string
     // const [owner, repo] = full_repository.split('/')
 
-    info(`payload ${payload}`)
+    const {
+      data: {tree}
+    } = await octokit.rest.git.getCommit({repo, owner, commit_sha})
+
+    const {
+      data: {sha: newSha}
+    } = await octokit.rest.git.createCommit({
+      repo,
+      owner,
+      parents: [commit_sha],
+      tree: tree.sha,
+      message,
+      author: {email}
+    })
+
+    await octokit.rest.git.updateRef({repo, owner, ref, sha: newSha})
+
+    info(`payload ${payload.repository}`)
   } catch (error) {
     if (error instanceof Error) setFailed(error.message)
   }
